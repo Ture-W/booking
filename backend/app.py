bc14caa30d8d5288c163a6d6e2b5880ed09be56c
Ture-W <121365446+Ture-W@users.noreply.github.com>
2024-10-03 19:14:39 +0200

    La till de första filerna

---
+++ backend/app.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from datetime import datetime, time, timedelta
+import requests as rq
+import time as t
+import urllib
+
+app = Flask(__name__)
+CORS(app, origins=['https://ture-w.github.io'])
+
+
+@app.route('/proxy/token', methods=['POST'])
+def get_token():
+  try:
+    data = request.get_json()
+    hh_username = data.get('hhUsername')
+    hh_password = data.get('hhPassword')
+
+    with rq.Session() as s:
+      url = "https://cloud.timeedit.net/hh/web/timeedit/sso/hh_saml2?back=https%3A%2F%2Fcloud.timeedit.net%2Fhh%2Fweb%2Fstudent%2Fauth%3Fredirect_url%3Dhttps%253A%252F%252Fuse.mazemap.com%252Foauthsuccess.html%253Fstate%253D%25257B%252522mazemap_redirect_uri%252522%25253A%252522https%25253A%25252F%25252Fuse.mazemap.com%25252F%252523v%25253D1%252526campusid%25253D278%252526zlevel%25253D1%252526center%25253D12.879858%25252C56.663184%252526zoom%25253D17.7%252526sharepoitype%25253Dpoi%252526sharepoi%25253D1000043720%252526showpoidetails%25253Dtrue%252522%25252C%252522provider%252522%25253A%252522time_edit%252522%25252C%252522providers%252522%25253A%25255B%252522time_edit%252522%25255D%25257D%2526token_handling%253Dtime_edit%26app%3Dtimeedit-mazemap%26id%3D4ceaffb327ef45e7b900415b14934d2d%26timestamp%3D1727875724%26sig%3D55ecf82fda9e958a909f2d34ab45e69ee0e35359"
+      s.get(url)
+
+      url = "https://idp.hh.se/idp/profile/SAML2/Redirect/SSO?execution=e1s1"
+      headers={
+        'Content-type':'application/json,application/x-www-form-urlencoded',
+        'Accept':'application/json'
+      }
+      params = {
+        "j_username": hh_username,
+        "j_password": hh_password,
+        "_eventId_proceed": True
+      }
+      response = s.post(url, params=params, headers=headers)
+
+      cookies = s.cookies.get_dict()
+      if not ("shib_idp_session" in cookies and "shib_idp_session_ss" in cookies):
+        return jsonify({"error": "HH login failed"})
+
+      url = "https://cloud.timeedit.net/hh/web/timeedit/ssoResponse/hh_saml2"
+      start = response.text.find('lue="')+5
+      end = response.text.find('"', start)
+      saml_resp = response.text[start:end]
+      params = { "SAMLResponse": saml_resp }
+      s.post(url, data=params, headers=headers)
+
+      url = s.cookies.get_dict()["sso-parameters"]
+      url = url[url.find("https"):url.find("&ssoserv")]
+      url = urllib.parse.unquote(url)
+
+      response = s.get(url)
+      return jsonify({"token": response.url[42:]})
+    
+  except Exception as e:
+    return jsonify({"error": str(e)})
+
+def book_room(poi_id: int, booking_start: time, duration: timedelta, token: str) -> dict:
+  url = "https://booking.mazemap.com/api/roombooking/bookroom/"
+  headers={
+    'Content-type':'application/json',
+    'Accept':'application/json'
+  }
+  booking_time = datetime.combine(datetime.now().date(), booking_start) - timedelta(hours=2)
+  start = booking_time.isoformat()+".000Z"
+  end = (booking_time+duration).isoformat()+".000Z"
+  payload = {
+    "poiid": poi_id,
+    "token": token,
+    "start": start,
+    "end": end,
+    "provider": "time_edit"
+  }
+  response = rq.post(url, json=payload, headers=headers)
+
+  try:
+    return response.json()
+  except:
+    return {"error": response.text, "success": False}
+
+@app.route('/proxy/book', methods=['POST'])
+def time_booking():
+  try:
+    data = request.get_json()
+    poi_id = data.get('poiId')
+    start_hour = data.get('startHour')
+    start_minute = data.get('startMinute')
+    duration = data.get('duration')
+    token = data.get('token')
+
+    while datetime.now().time().hour < 6: t.sleep(0.01)
+
+    return jsonify(book_room(poi_id, time(start_hour, start_minute), timedelta(minutes=duration), token))
+  
+  except Exception as e:
+    return jsonify({"error": str(e), "success": False})
+
+@app.route('/proxy/test', methods=['POST'])
+def test():
+  json = {
+    "time": datetime.now().isoformat()+".000Z"
+  }
+  return jsonify(json)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
---
+++ index.js
@@ -0,0 +1,103 @@
+
+async function getPoi(name) {
+  const url = "https://search.mazemap.com/search/equery/";
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+  const params = {
+    "q": name,
+    "rows": 1,
+    "withpois": true
+  };
+  
+  try {
+    const response = await $.ajax({
+      url: url,
+      method: 'GET',
+      headers: headers,
+      data: params
+    });
+    return response.result[0].poiId;
+
+  } catch (error) {
+    console.error('Fel uppstod i getPoi:', error);
+    return null;
+  }
+}
+
+async function getToken(hhUsername, hhPassword) {
+  const url = "https://booking-tp6b.onrender.com/proxy/token";
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+  const params = {
+    "hhUsername": hhUsername,
+    "hhPassword": hhPassword
+  };
+
+  try {
+    const response = await $.ajax({
+      url: url,
+      method: 'POST',
+      headers: headers,
+      data: params
+    });
+
+    if (response.error) {
+      console.error('Fel uppstod i getToken:', error);
+      return null;
+    }
+    return response.token
+
+  } catch (error) {
+    console.error('Fel uppstod i getToken:', error);
+    return null;
+  }
+}
+
+async function bookRoom(poiId, startHour, startMinute, duration, token) {
+  const url = "https://booking-tp6b.onrender.com/proxy/book";
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+  const params = {
+    "action": "book",
+    "poiId": poiId,
+    "startHour": startHour,
+    "startMinute": startMinute,
+    "duration": duration,
+    "token": token
+  };
+
+  try {
+    const response = await $.ajax({
+      url: url,
+      method: 'POST',
+      headers: headers,
+      data: params
+    });
+
+    return response
+
+  } catch (error) {
+    console.error('Fel uppstod i bookRoom:', error);
+    return null;
+  }
+}
+
+function parseBookingError(errorText) {
+  switch (errorText) {
+    case "busy":
+      return "Rummet är redan bokat under en tid som krockar med din önskade tid";
+    case "not_allowed_interval":
+      return "Den valda perioden är inte tillåten för detta rummet eller din önskade tid är inte en intervall av tillåtna perioder för bokning." +
+      " Detta felet kan också hända om din valda tid redan passerat eller om bokningen försöktes under en otillåten tid (t.ex. innan kl 08:00).";
+    case "time_quota_exceeded":
+      return "Du har redan ett eller flera bokade rum för totalt 2 timmar.";
+    default:
+      return "Okänt fel.";
+  }
+}
